f510cd2413f1d7bbb77d81193d8a4430c3db91b5
noppharutCode <tee_lnw55@hotmail.com>
2017-04-05 15:41:39 +0700

    update : map

---
+++ Project/platforms/android/assets/www/js/main.js
@@ -0,0 +1,30 @@
+var map;
+document.addEventListener("deviceready", function() {
+  // Define a div tag with id="map_canvas"
+  var mapDiv = document.getElementById("map_canvas");
+
+  console.log(mapDiv);
+  // Initialize the map plugin
+
+  map = plugin.google.maps.Map.getMap(mapDiv);
+ 
+
+
+  // You have to wait the MAP_READY event.
+  map.on(plugin.google.maps.event.MAP_READY, onMapInit);
+
+});
+
+function onMapInit(map) {
+
+	  map.animateCamera({
+
+  'tilt': 60,
+  'zoom': 20,
+  'bearing': 140
+});
+	alert("My map");
+
+}
+
+
--- Project/platforms/android/src/TU/Travel/MainActivity.java
+++ Project/platforms/android/src/TU/Travel/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package TU.Travel;
+package tu.travel;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
